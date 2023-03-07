41c8ae7587d2a0fbed124a91ebeaad31538c3798
Schweidzon <dannielsch@gmail.com>
2023-03-07 19:14:18 -0300

    delete: rota e controller de post para dar like (estava errado)

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -1,6 +1,2 @@
 import db from '../databases/db.js'
 
-export async function like(id) {
-    return db.query(`UPDATE posts SET liked = true WHERE id = $1`, [id])
-
-}
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import { likePost } from "../controllers/postControllers.js";
+
 
 const postsRouter = Router()
 
-postsRouter.put("/like/:id", likePost) // depois passar essa funcionalidade para dentro do controller de edit post (eu acho)
 
 export default postsRouter
