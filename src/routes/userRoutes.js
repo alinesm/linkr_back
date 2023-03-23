dba42a6cb87512738209cc37e9710f2f15dd249d
Alexandre de Freitas <alexandrefsoficial@gmail.com>
2023-03-23 15:05:08 -0300

    feature/ find follow relation

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { followUser, getAllUsers, getUserPosts, unfollowUser } from "../controllers/userControllers.js";
+import { findFollowRelation, followUser, getAllUsers, getUserPosts, unfollowUser } from "../controllers/userControllers.js";
 import { followSchema } from '../schema/userSchema.js';
 import { validateSchema } from '../middlewares/validateSchema.js';
 
@@ -12,6 +12,7 @@ userRoutes.get("/users", getAllUsers)
 userRoutes.get("/users/:id", getUserPosts)
 userRoutes.post("/follow", validateSchema(followSchema), followUser)
 userRoutes.delete("/unfollow",validateSchema(followSchema), unfollowUser)
+userRoutes.get("/find_follow", validateSchema(followSchema), findFollowRelation)
 
 export default userRoutes
 
